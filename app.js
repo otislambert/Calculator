3cae695a3057d3dc5679f9222c721abafcf30e6e
Otis Lambert <otislambert@gmail.com>
2021-12-05 21:35:25 -0800

    addition works

--- app.js
+++ app.js
@@ -4,43 +4,11 @@ const calcScreen = document.querySelector('#screen-text');
 const numbtns = Array.from(document.querySelectorAll('.number'));
 const operators = Array.from(document.querySelectorAll('.operator'));
 const clearBtn = document.querySelector('#clear');
+const equalBtn = document.querySelector('#equals');
 
+// sets the stage for functions
 let operation = 'place';
-
-function placeNum(e) {
-	let screenContent = calcScreen.textContent;
-	let result = screenContent + e.target.id;
-	calcScreen.textContent = result;
-	console.log(result);
-}
-
-function addition(num) {
-	let a = calcScreen.textContent;
-	result = parseFloat(a) + num;
-	calcScreen.textContent = result;
-}
-
-function subtraction(num) {
-	let a = calcScreen.textContent;
-	let result = parseFloat(a) - num;
-	calcScreen.textContent = result;
-}
-
-function multiplication(num) {
-	let a = calcScreen.textContent;
-	let result = parseFloat(a) * num;
-	calcScreen.textContent = result;
-}
-
-function division(num) {
-	let a = calcScreen.textContent;
-	let result = parseFloat(a) / num;
-	calcScreen.textContent = result;
-}
-
-function clear() {
-	calcScreen.textContent = '';
-}
+let a = '';
 
 numbtns.forEach(btn => btn.addEventListener('click', placeNum));
 
@@ -48,15 +16,57 @@ operators.forEach(btn => btn.addEventListener('click', setOperation));
 
 clearBtn.addEventListener('click', clear);
 
+equalBtn.addEventListener('click', operate);
+
 function setOperation(e) {
+	a = calcScreen.textContent;
 	operation = e.target.id;
+	calcScreen.textContent = e.target.id;
 }
 
-function operate(e) {
-	if (operation == 'place') {
-		placeNum(e);
-	}
+function operate() {
+	console.log('operate')
 	if (operation == 'add') {
 		addition();
 	}
+}
+
+const operations = ['add', 'subtract', 'multiply', 'divide'];
+
+function placeNum(e) {
+	let result = '';
+	let screenContent = calcScreen.textContent;
+	if (operations.includes(calcScreen.textContent)) {
+		result = e.target.id;
+	}
+	else {
+		result = screenContent + e.target.id;
+	}
+	calcScreen.textContent = result;
+	console.log(result);
+}
+
+// calculator operations
+function addition() {
+	result = parseFloat(a) + parseFloat(calcScreen.textContent);
+	calcScreen.textContent = result;
+}
+
+function subtraction() {
+	let result = parseFloat(a) - parseFloat(calcScreen.textContent);
+	calcScreen.textContent = result;
+}
+
+function multiplication() {
+	let result = parseFloat(a) * parseFloat(calcScreen.textContent);
+	calcScreen.textContent = result;
+}
+
+function division() {
+	let result = parseFloat(a) / parseFloat(calcScreen.textContent);
+	calcScreen.textContent = result;
+}
+
+function clear() {
+	calcScreen.textContent = '';
 }
